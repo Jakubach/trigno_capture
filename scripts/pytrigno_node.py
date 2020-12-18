2148ca8f37247d9589dcb9b0447cc6c3abbccbdb
Jakubach <jakubach@gmail.com>
2020-12-18 13:52:18 +0100

    Update pytrigno_node.py

--- scripts/pytrigno_node.py
+++ scripts/pytrigno_node.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import rospy
-from pytrigno.pytrigno import TrignoOrientation
+from pytrignos.pytrignos import TrignoAdapter
 
 
 class TrignoCapture:
     def __init__(self):
-        self.trigno_sensor = TrignoOrientation(channel_range = (0,2), samples_per_read = 100)
+        self.trigno_sensors = TrignoAdapter()
+        self.trigno_sensors.add_sensors(sensors_mode='ORIENTATION', sensors_numbers=(1,), sensors_labels=('ORIENTATION1',))
+    def start(self):
+        self.trigno_sensors.start_acquisition()
     def stop(self):
-        self.trigno_sensor.stop()
+        self.trigno_sensors.stop_acquisition()
         
         
 if __name__ == "__main__":
-    rospy.init_node("emg_capture")
+    rospy.init_node("trigno_capture")
     trigno_capture = TrignoCapture()
+    trigno_capture.start()
     rospy.on_shutdown(trigno_capture.stop)
     rospy.loginfo("Sensor node is ready.")
     rospy.spin()
